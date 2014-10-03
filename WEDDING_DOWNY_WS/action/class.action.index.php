d649d0ac72529dd2abbdbca5c5a821cf6454e294
downyws@gmail.com <downyws@gmail.com@f678556b-1e9a-66a2-00e0-119e0717744e>
2014-10-03 03:23:13 +0000

    wedding 更新

--- WEDDING_DOWNY_WS/action/class.action.index.php
+++ WEDDING_DOWNY_WS/action/class.action.index.php
@@ -13,9 +13,9 @@ class ActionIndex extends Action
 
 	public function methodMap()
 	{
-		$params = $this->_submit->obtain($_REQUEST, array(
-			'location' => array(array('valid', 'in', null, 'x', array('x', 'j')))
-		));
+		$params = $this->_submit->obtain($_REQUEST, [
+			'location' => [['valid', 'in', null, 'x', ['x', 'j']]]
+		]);
 		if($params['location'] == 'j')
 		{
 			Front::redirect(MAP_URL_J);
--- WEDDING_DOWNY_WS/action/class.action.mobi_index.php
+++ WEDDING_DOWNY_WS/action/class.action.mobi_index.php
@@ -8,9 +8,9 @@ class ActionMobi_Index extends ActionIndex
 
 	public function methodWeixinMap()
 	{
-		$params = $this->_submit->obtain($_REQUEST, array(
-			'location' => array(array('valid', 'in', null, 'x', array('x', 'j')))
-		));
+		$params = $this->_submit->obtain($_REQUEST, [
+			'location' => [['valid', 'in', null, 'x', ['x', 'j']]]
+		]);
 		$this->assign('params', $params);
 	}
 }
